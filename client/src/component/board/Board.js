2dc8532ffea7df599ec3ac96c4709c7bd39fd626
KACPER SINILO <kacperosinilo@gmail.com>
2023-02-01 18:44:37 +0100

    creating react components

---
+++ client/src/component/board/Board.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Board() {
+    return (
+    
+        <div id="container">
+            <div id="chessBoard" class="d-flex justify-content-center">
+            
+            </div>
+        </div>
+      
+    );
+  }
