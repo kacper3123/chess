fc8ab069e27e506ff4564e1957dea89ecd7f84b1
KACPER SINILO <kacperosinilo@gmail.com>
2023-02-03 14:04:22 +0100

    added pieces to the board

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,8 @@
-import React, {useState, useEffect} from 'react'
+// import React, {useState, useEffect} from 'react'
 
 function App() {
-  const [data, setData] = useState([{}])
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  }, [])
   
-  return (
-    <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ):(
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
-    </div>
-  )
+  return (<div class="br"></div>)
 }
 
 export default App
